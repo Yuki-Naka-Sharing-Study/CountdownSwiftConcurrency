1f5a831548751b2023ef848f6460bb4154edd4bd
仲優樹 <yu-ki216m@outlook.jp>
2025-06-28 08:49:18 +0900

    Initial Commit

---
+++ CountdownSwiftConcurrency/CountdownSwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownSwiftConcurrencyApp.swift
+//  CountdownSwiftConcurrency
+//
+//  Created by 仲優樹 on 2025/06/28.
+//
+
+import SwiftUI
+
+@main
+struct CountdownSwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
